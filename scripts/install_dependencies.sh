2884fb3946e34922670bb784293bcdb5cd72c6cb
pratheekp1904 <pratheekpganiga@gmail.com>
2024-09-14 02:44:29 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,4 @@
 cd /home/ubuntu/speech-translator
 python3 -m venv venv
 source venv/bin/activate
-pip install streamlit speech_recognition googletrans==4.0.0-rc1 pyttsx3
+pip install streamlit speech_recognition googletrans==4.0.0-rc1 pyttsx3 pyaudio
